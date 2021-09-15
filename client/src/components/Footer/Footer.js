b69d2ac5e80e95c079c2a203fee9651167b1b1ad
Parham-S <psaniei@hotmail.com>
2021-09-15 15:53:33 -0400

    links social

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,24 +1,38 @@
 import React from "react";
-import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
+import { MDBBtn, MDBIcon } from "mdb-react-ui-kit";
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <footer className="footer"> 
-    
-      <MDBBtn tag='a' color='none' className='m-2' style={{ color: '#111166' }} href='https://github.com/Parham-S'>
-        <MDBIcon fab icon='github' size='2x' />
-      </MDBBtn>
+	return (
+		<footer className='footer'>
+			<MDBBtn
+				tag='a'
+				color='none'
+				className='m-2'
+				style={{ color: "#111166" }}
+				href='https://github.com/Parham-S'>
+				<MDBIcon fab icon='github' size='2x' />
+			</MDBBtn>
 
-      <MDBBtn tag='a' color='none' className='m-2' style={{ color: '#111166' }}>
-        <MDBIcon fab icon='dev' size='2x' />
-      </MDBBtn>
+			<MDBBtn
+				tag='a'
+				color='none'
+				className='m-2'
+				style={{ color: "#111166" }}
+				href='https://parhamsaniei.com'>
+				<MDBIcon fab icon='dev' size='2x' />
+			</MDBBtn>
 
-      <MDBBtn tag='a' color='none' className='m-2' style={{ color: '#111166' }}>
-        <MDBIcon fab icon='linkedin-in' size='2x' />
-      </MDBBtn>
-      </footer>
-  );
-}
+			<MDBBtn
+				tag='a'
+				color='none'
+				className='m-2'
+				style={{ color: "#111166" }}
+				href='https://www.linkedin.com/in/psaniei/'>
+				<MDBIcon fab icon='linkedin-in' size='2x' />
+			</MDBBtn>
+		</footer>
+	);
+};
 
 export default Footer;
